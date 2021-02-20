c9a805102dcbebba2531433b47302934b87a99bc
Griffin Dube <gdube@g.clemson.edu>
2021-02-20 10:24:42 -0500

    Adding compilation script for k20, p100, v100 gpus

---
+++ compileall.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+[ -f '~/hpcgpu/ece8780_p2/build/' ] || mkdir build
+rm -rf build/*
+
+for bsize in 4 8 16 32
+do
+	sed -i "s/BLOCK=.*/BLOCK=${bsize}/" Makefile
+	for sm in 35 60 70
+	do
+		sed -i "s/sm\_../sm\_$sm/" Makefile
+		make clean
+		make
+		mv gblur ./build/gblur_${sm}_${bsize}
+	done
+done
+
